e4b558954facf46c715ee0c51d70512a1f46d991
Yogesh240901 <yogeshwaran240901@gmail.com>
2025-09-17 13:59:35 +0530

    op not display

--- app/src/main/java/com/chat/app/model/ChatMessage.java
+++ app/src/main/java/com/chat/app/model/ChatMessage.java
@@ -7,6 +7,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChatMessage {
 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 	private Long id;
 	private String sender;
 	private String content;
